4c36fd09562d97046516196d2ee2fe2463c8a1d2
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-09-17 12:16:25 -0300

    Configuracoes e ajustes

--- app/classes/costumer/costumer.js
+++ app/classes/costumer/costumer.js
@@ -4,58 +4,49 @@
 export default class Costumer {
   id: number;
 
-  name: string;
-
   table: number;
 
   status: string;
 
   /**
-   * Construtor da classe cliente. Utilizado para criar novos clientes.
-   * @param {string} newName - Nome do cliente.
+   * @function constructor
+   * @description Construtor da classe cliente. Utilizado para criar novos clientes.
+   * @param {number} newId - Id do cliente.
    * @param {number} newTable - Número da mesa do cliente.
    * @param {string} newStatus - Status atual do cliente. podendo ser: Aguardando atendimento, Pedido em andamento, Pedido entregue ou finalizado
    */
-  constructor(
-    newId: number,
-    newName: string,
-    newTable: number,
-    newStatus: string
-  ) {
+  constructor(newId: number, newTable: number, newStatus: string) {
     this.id = newId;
-    this.name = newName;
     this.table = newTable;
     this.status = newStatus;
   }
 
   /**
-   * Retorna as informações do cliente.
-   * @return {array} Retorna uma lista com o nome, número da mesa e status do cliente, respectivamente.
+   * @function GetCostumer
+   * @description Retorna as informações do cliente.
+   * @return {array} Retorna uma lista com o id, número da mesa e status do cliente, respectivamente.
    */
-  GetCostumer(): Array<*> {
-    return [this.name, this.table, this.status];
-  }
 
   /**
-   * Altera o nome do cliente.
-   * @param {string} newName - Novo nome do cliente.
-   */
-  EditName(newName: string) {
-    this.name = newName;
-  }
-
-  /**
-   * Altera o número da mesa do cliente.
+   * @function EditTable
+   * @description Altera o número da mesa do cliente.
    * @param {number} newTable - Novo número da mesa do cliente.
    */
+
+  /**
+   * @function EditStatus
+   * @description Altera o status do cliente.
+   * @param {string} newStatus - Novo status do cliente.
+   */
+
+  GetCostumer(): Array<*> {
+    return [this.id, this.table, this.status];
+  }
+
   EditTable(newTable: number) {
     this.table = newTable;
   }
 
-  /**
-   * Altera o status do cliente.
-   * @param {string} newStatus - Novo status do cliente.
-   */
   EditStatus(newStatus: string) {
     this.status = newStatus;
   }
